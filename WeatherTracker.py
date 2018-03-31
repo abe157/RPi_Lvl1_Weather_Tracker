743e7c916d4a12ae2383e8e025a96a4a8e43d655
abe157 <abenezertgs96@gmail.com>
2018-03-31 09:03:33 -0400

    Cleared key information

--- WeatherTracker.py
+++ WeatherTracker.py
@@ -32,13 +32,13 @@ time_format = 12 # 12 or 24
 date_format = "%b %d, %Y" # check python doc for strftime() for options
 news_country_code = 'us'
 # WEATHER: account from https://darksky.net/ 
-weather_api_token = '7d1611b46723c93de239aab1676c7129' #This is your Secret key
+weather_api_token = '' #This is your Secret key
 weather_lang = 'en' 
 weather_unit = 'us'
 # Set this if IP location lookup does not work for you (must be a string)
 # Set this if IP location lookup does not work for you (must be a string)
-latitude = '37.8267'
-longitude = '-122.4233'
+latitude = ''
+longitude = ''
 # Feel Free to play around with different text sizes
 xlarge_text_size = 40#94
 large_text_size = 30#48
