05123868ca34dd4265909680e82b70ce226a0ffa
Abenezer Wudenhe <abenezertgs96@gmail.com>
2018-03-31 16:04:28 -0400

    Updated WeatherTracker.py

--- WeatherTracker.py
+++ WeatherTracker.py
@@ -17,10 +17,10 @@ from contextlib import contextmanager
 import httplib2
 import os
 
-from apiclient import discovery
-from oauth2client import client
-from oauth2client import tools
-from oauth2client.file import Storage
+#from apiclient import discovery
+#from oauth2client import client
+#from oauth2client import tools
+#from oauth2client.file import Storage
 
 import datetime
 #=================================================
